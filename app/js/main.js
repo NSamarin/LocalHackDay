9888105040f664d213e8b8f722f395b7778b7824
just-barcodes <justbarcodes@gmail.com>
2015-10-10 11:47:28 +0100

    index

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,25 @@
 
+
+
+
+
+
+
+$(document).ready(function() {
+
+
+    function displayCourseInfo(courseData, table) {
+        table.append(courseData.name);
+    }
+
+
+    var pageElement = $("#display");
+    var courseData = {'name': 'name test'};
+
+    displayCourseInfo(courseData, pageElement);
+
+});
+
+
+
+
