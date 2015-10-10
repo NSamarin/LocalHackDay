0b8905b7421465db0c066accda369b4b79713a4b
just-barcodes <justbarcodes@gmail.com>
2015-10-10 12:33:36 +0100

    onclick

--- app/js/main.js
+++ app/js/main.js
@@ -13,18 +13,25 @@ $(document).ready(function() {
     }
 
     function makeRow(courseData) {
-        var rowString = "<tr>";
+        var rowString = "<tr class='tablerow'>";
         rowString += "<td>" + courseData.name + "</td><td>" + courseData.type + "</td><td>" + courseData.time + "</td>";
         return rowString + "<tr>";
     }
 
-
-    var pageElement = $("#tablebody");
+    var tableBody = $("#tablebody");
     var courseData = {'name': 'name test', 'type': 'lecture', 'time': 12345};
 
-    displayCourseInfo(courseData, pageElement);
-    displayCourseInfo(courseData, pageElement);
-    displayCourseInfo(courseData, pageElement);
+    displayCourseInfo(courseData, tableBody);
+    displayCourseInfo(courseData, tableBody);
+    displayCourseInfo(courseData, tableBody);
+    var rowElements = $('.tablerow');
+
+
+    for (var i=0; i<rowElements.length; i++) {
+        rowElements.on("click", function() {
+            alert(i);
+        });
+    }
 
 });
 
